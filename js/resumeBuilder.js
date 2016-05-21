de3bd90fec963e0f850a788434da3578b6fef7ca
ronen sebag <sebag_r@yahoo.com>
2016-05-21 19:10:17 +0300

    שינויים קטנים בקבצים

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+$("#main").append("css & html   <br>");
+
+
+var firstName="ronen";
+console.log(firstName);
+
+var owesometoughte="i am ronen and i am owesome";
+console.log(owesometoughte);
+
+var email="sebag_r@yahoo.com";
+var newemail =email.replace("yahoo","gmail");
+console.log(email);
+console.log(newemail);
+var funthoughts=owesometoughte.replace("owesome" , "fun");
+$("#main").append(funthoughts);
